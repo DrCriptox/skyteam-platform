ad2de786425188c5c96b4e1b0d65d4b222d82bec
DrCriptox <yonfer.300@gmail.com>
2026-04-05 17:49:16 -0500

    Rank icons: use ◆ unicode diamond that inherits CSS color (purple/blue/red/green per rank)

--- sky-team.js
+++ sky-team.js
@@ -730,8 +730,8 @@ function renderSTArbol() {
   html += '<select id="st-filter-rank" onchange="stState.treeFilterRank=this.value;_refreshTree()" style="' + _filterSelectCSS() + '">';
   html += '<option value="all">Rango: Todos</option>';
   var rankOptions = [
-    {v:'0',l:'\u26AA Cliente'},{v:'1',l:'\uD83E\uDD48 INN 200'},{v:'2',l:'\uD83D\uDD37 INN 500'},
-    {v:'3',l:'\uD83D\uDD37 NOVA 1500'},{v:'4',l:'\uD83D\uDD37 NOVA 5K'},{v:'5',l:'\uD83D\uDD37 NOVA 10K'},
+    {v:'0',l:'\u26AA Cliente'},{v:'1',l:'\uD83E\uDD48 INN 200'},{v:'2',l:'\u25C6 INN 500'},
+    {v:'3',l:'\u25C6 NOVA 1500'},{v:'4',l:'\u25C6 NOVA 5K'},{v:'5',l:'\u25C6 NOVA 10K'},
     {v:'6',l:'\uD83D\uDC8E NOVA DIAMOND'},{v:'7',l:'\uD83D\uDC8E\uD83D\uDC8E NOVA 50K'},{v:'8',l:'\uD83D\uDC8E\uD83D\uDC8E\uD83D\uDC8E NOVA 100K'}
   ];
   rankOptions.forEach(function(ro) {
